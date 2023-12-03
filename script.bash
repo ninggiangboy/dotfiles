38e48e8e39d6946e30f9229cb42f03355c5a9914
Duy Khanh <ninggiangboy@gmail.com>
2023-12-03 22:00:30 +0700

    wallpaper

--- script.bash
+++ script.bash
@@ -4,19 +4,19 @@ red=$(tput setaf 3)
 reset=$(tput sgr0)
 
 if [ "$EUID" -ne 0 ]; then
-  echo "${red}Run script with root permissions.${reset}"
+  echo "${yellow}Run script with root permissions.${reset}"
   exit 1
 fi
 
-echo "${red}Dnf config...${reset}"
+echo "${yellow}Dnf config...${reset}"
 echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 echo "deltarpm=true" >> /etc/dnf/dnf.conf
 
-echo "${red}Update and upgrade system...${reset}"
+echo "${yellow}Update and upgrade system...${reset}"
 dnf -y update && dnf -y upgrade
 
-echo "${red}Install Docker...${reset}"
+echo "${yellow}Install Docker...${reset}"
 dnf -y remove docker \
     docker-client \
     docker-client-latest \
@@ -36,30 +36,30 @@ sudo chmod 666 /var/run/docker.sock
 
 mkdir ~/.config/
 
-echo "${red}Install kitty...${reset}"
+echo "${yellow}Install kitty...${reset}"
 dnf -y install kitty
 cp -r kitty ~/.config/
 
-echo "${red}Install zsh...${reset}"
+echo "${yellow}Install zsh...${reset}"
 dnf -y install zsh
 chsh -s $(which zsh)
-echo "${red}Install oh-my-zsh...${reset}"
+echo "${yellow}Install oh-my-zsh...${reset}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "${red}Install zsh plugins...${reset}"
+echo "${yellow}Install zsh plugins...${reset}"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 cp .zshrc ~/
 
-echo "${red}Git config...${reset}"
+echo "${yellow}Git config...${reset}"
 cp .gitconfig ~/
 
-echo "${red}Install Flatpak...${reset}"
+echo "${yellow}Install Flatpak...${reset}"
 dnf -y install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-echo "${red}Install some app...${reset}"
+echo "${yellow}Install some app...${reset}"
 dnf -y install neofetch
 dnf -y install btop
 dnf -y install cava
@@ -76,5 +76,5 @@ flatpak -y install flathub org.qbittorrent.qBittorrent
 flatpak -y install flathub com.anydesk.Anydesk
 flatpak -y install flathub com.visualstudio.code
 
-echo "${red}Install Pop Shell...${reset}"
+echo "${yellow}Install Pop Shell...${reset}"
 dnf -y install gnome-shell-extension-pop-shell xprop
